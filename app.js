df13e9771cb06b39fb776910da8290e622c7d12d
omar.chacin <omar.chacin@sigmagroup.no>
2016-09-05 10:28:43 +0200

    add EJS

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+// app.use(express.static('src/views'));
+app.set('views', './src/views');
+// app.set('view engine', 'jade');
 
-app.use(express.static('src/views'));
+// var handlebars = require('express-handlebars');
+// app.engine('.hbs', handlebars({extname: '.hbs'}));
+//
+// app.set('view engine', '.hbs');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello');
+  res.render('index', {
+    title: 'Hello from renders',
+    list: ['a', 'b']
+  });
 });
 
 app.get('/books', function (req, res) {
